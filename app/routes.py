ea8b46600be7416e8792a7f6b0e667081f04f422
Ariane <ariane.fairlie@gmail.com>
2020-11-09 20:03:34 -0500

    login/logout working

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, redirect, flash
 from app import app
 from app.forms import LoginForm, RegisterForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 @app.route('/')
 def index():
@@ -19,9 +21,15 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+  if current_user.is_authenticated:
+    return redirect('/')
   form = LoginForm()
   if form.validate_on_submit():
-    flash(f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}")
+    user = User.query.filter_by(username=form.username.data).first()
+    if user is None or not user.check_password(form.password.data):
+      flash('Invalid username or password')
+      return redirect('/login')
+    login_user(user, remember=form.remember_me.data)
     return redirect('/')
   return render_template('login.html', title='Sign In', form=form)
 
@@ -32,6 +40,11 @@ def register():
     flash(f"{form.username.data} registered with email: {form.email.data}")
     return redirect('/')
   return render_template('login.html', title='Register', form=form)
+
+@app.route('/logout')
+def logout():
+  logout_user()
+  return redirect('/')
 
 @app.route('/hello')
 def hello():
