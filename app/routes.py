3271c9b520f19218ab9773b587e1e6849518f352
Ariane <ariane.fairlie@gmail.com>
2020-11-09 21:51:26 -0500

    implemented redirect for secure page if not logged in

--- app/routes.py
+++ app/routes.py
@@ -1,36 +1,29 @@
-from flask import render_template, redirect, flash
+from flask import render_template, redirect, flash, url_for, request
 from app import app
 from app.forms import LoginForm, RegisterForm
-from flask_login import current_user, login_user, logout_user
-from app.models import User
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User, Post
+from werkzeug.urls import url_parse
 
 @app.route('/')
 def index():
-  user = {'username': 'Ariane'}
-  posts = [
-    {
-        'author': {'username': 'John'},
-        'body': 'Beautiful day in Portland!'
-    },
-    {
-        'author': {'username': 'Susan'},
-        'body': 'The Avengers movie was so cool!'
-    }
-  ]
-  return render_template('index.html', title='Home', user=user, posts=posts)
+  return render_template('index.html', title='Home', posts=Post.query.all())
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   if current_user.is_authenticated:
-    return redirect('/')
+    return redirect(url_for('index'))
   form = LoginForm()
   if form.validate_on_submit():
     user = User.query.filter_by(username=form.username.data).first()
     if user is None or not user.check_password(form.password.data):
       flash('Invalid username or password')
-      return redirect('/login')
+      return redirect(url_for('login'))
     login_user(user, remember=form.remember_me.data)
-    return redirect('/')
+    next_page = request.args.get('next')
+    if not next_page or url_parse(next_page).netloc != '':
+      next_page = url_for('index')
+    return redirect(next_page)
   return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -44,8 +37,9 @@ def register():
 @app.route('/logout')
 def logout():
   logout_user()
-  return redirect('/')
+  return redirect(url_for('index'))
 
-@app.route('/hello')
-def hello():
-  return render_template('hello from the app')
+@app.route('/secure')
+@login_required
+def secure():
+  return render_template('secure.html')
